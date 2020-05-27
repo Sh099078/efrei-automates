d043e124406e52b9ffc56adc53b58ebca97c74d1
Loup Federico <loup.federico@gmail.fr>
2020-05-27 19:07:00 +0000

    [MAIN] add basic main program loop

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,13 @@
 #include <iostream>
 
+#include "read-automaton.hh"
+#include "automaton.hh"
+
 int main(void) {
+  size_t auto_nb;
+  while(std::cout << "Which automaton do you want to test ?\n" && std::cin >> auto_nb) {
+    std::cout << "You chose the automaton " << auto_nb << "." << std::endl;
+  }
+  std::cout << "Leaving the program." << std::endl;
   return 0;
 }
