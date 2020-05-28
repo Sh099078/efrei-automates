37326c2d80bed25ebbbaf81741aaac4ab671ffab
Loup Federico <loup.federico@gmail.fr>
2020-05-28 10:39:18 +0000

    [AUTOMATON_HH] add toString method declaration

--- headers/automaton.hh
+++ headers/automaton.hh
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 class automaton {
   public:
     automaton(int, int, int, std::vector<std::vector<char>>);
+    std::string toString();
   private:
     int nb_symbols_;
     int nb_states_;
