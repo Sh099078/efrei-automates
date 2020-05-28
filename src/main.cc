2b542b208fd3884d7506d2b605e11b6c3a36d0e8
Loup Federico <loup.federico@gmail.fr>
2020-05-28 13:12:30 +0000

    [MAIN_CC] remove hardcoded .txt automatons pathnames

--- src/main.cc
+++ src/main.cc
@@ -3,10 +3,18 @@
 #include "read-automaton.hh"
 #include "automaton.hh"
 
+#define TEAM_NB 24
+
 int main(void) {
-  size_t auto_nb;
-  while(std::cout << "Which automaton do you want to test ?\n" && std::cin >> auto_nb) {
-    std::cout << "You chose the automaton " << auto_nb << "." << std::endl;
+  size_t choice;
+  while(std::cout << "Which automaton do you want to test ?\n" && std::cin >> choice) {
+    std::cout << "You chose the automaton " << choice << "." << std::endl;
+    std::string pathname = "tests/L3New-MpI-"
+      + std::to_string(TEAM_NB) + "-"
+      + std::to_string(choice) + ".txt";
+    Automaton automaton = readAutomaton(pathname.c_str());
+    //TODO Automaton processing
+    std::cout << '\n' << automaton.toString() << '\n';
   }
   std::cout << "Leaving the program." << std::endl;
   return 0;
