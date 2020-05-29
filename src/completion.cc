a89631d6980f7591170e47c7af43083fa6d8b7b0
Loup Federico <loup.federico@gmail.fr>
2020-05-29 21:34:30 +0000

    [COMPLETION] implement is_complete()

--- src/completion.cc
+++ src/completion.cc
@@ -1,8 +1,13 @@
 #include "completion.hh"
 
-bool is_complete(Automaton) {
-  //TODO
-  return false;
+bool is_complete(Automaton automaton) {
+  auto& transitions = automaton.transitions_;
+  for (auto it = transitions.begin(); it != transitions.end(); it++) {
+    for (auto jt = (*it).begin(); jt != (*it).end(); jt++)
+      if ((*jt).size() == 0)
+        return false;
+  }
+  return true;
 }
 
 Automaton complete(Automaton) {
