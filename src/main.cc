faeb4c286dcd9ca2c6aaa0e3e0d18dcbea008abe
Loup Federico <loup.federico@gmail.fr>
2020-06-03 18:08:01 +0000

    [MAIN_CC] add complementarization to main loop

--- src/main.cc
+++ src/main.cc
@@ -6,6 +6,7 @@
 #include "automaton.hh"
 #include "completion.hh"
 #include "word-recognition.hh"
+#include "complementarization.hh"
 
 #define TEAM_NB 24
 
@@ -18,18 +19,20 @@ int main(void) {
       + std::to_string(choice) + ".txt";
     Automaton automaton = readAutomaton(pathname.c_str());
     //TODO Automaton processing
-    std::cout << "Is complete : " << std::boolalpha << is_complete(automaton) << std::endl;
     std::cout << '\n' << automaton.toString() << '\n';
-    std::cout << "Completion : " << std::endl;
-    automaton = complete(automaton);
-    //
-    std::cout << '\n' << automaton.toString() << '\n';
-    std::cout << "Type a words to recognize separated by whitespaces: ";
+    std::cout << "Type words to recognize separated by whitespaces: ";
     std::string line;
     std::cin >> line;
-
-      std::cout << "word '" << line << "' recognized : " << std::boolalpha
-        << recognize_word(automaton, line) << std::endl;
+    std::cout << "word '" << line << "' recognized : " << std::boolalpha
+      << recognize_word(automaton, line) << std::endl;
+    std::cout << "Is complete : " << std::boolalpha << is_complete(automaton) << std::endl;
+    std::cout << "Completion : " << std::endl;
+    Automaton a_complete = complete(automaton);
+    //
+    std::cout << '\n' << a_complete.toString() << '\n';
+    std::cout << "Complementary automaton : " << std::endl;
+    auto a_complementary = complementary(a_complete);
+    std::cout << '\n' << a_complementary.toString() << '\n';
   }
   std::cout << "Leaving the program." << std::endl;
   return 0;
