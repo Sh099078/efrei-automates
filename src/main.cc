d80d74b67851848ab5384f5462944d15547a2257
Loup Federico <loup.federico@gmail.fr>
2020-06-03 17:11:46 +0000

    [MAIN_CC] main loop : add completion and word recognition

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "read-automaton.hh"
 #include "automaton.hh"
+#include "completion.hh"
+#include "word-recognition.hh"
 
 #define TEAM_NB 24
 
@@ -14,7 +18,18 @@ int main(void) {
       + std::to_string(choice) + ".txt";
     Automaton automaton = readAutomaton(pathname.c_str());
     //TODO Automaton processing
+    std::cout << "Is complete : " << std::boolalpha << is_complete(automaton) << std::endl;
     std::cout << '\n' << automaton.toString() << '\n';
+    std::cout << "Completion : " << std::endl;
+    automaton = complete(automaton);
+    //
+    std::cout << '\n' << automaton.toString() << '\n';
+    std::cout << "Type a words to recognize separated by whitespaces: ";
+    std::string line;
+    std::cin >> line;
+
+      std::cout << "word '" << line << "' recognized : " << std::boolalpha
+        << recognize_word(automaton, line) << std::endl;
   }
   std::cout << "Leaving the program." << std::endl;
   return 0;
